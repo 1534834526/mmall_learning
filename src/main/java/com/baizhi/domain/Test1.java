20d769ec17375de3c4a88385b146c12e1e97fea9
1534834526 <1534834526@qq.com>
2018-06-12 12:47:40 +0800

    first commit init project‘

---
+++ src/main/java/com/baizhi/domain/Test1.java
@@ -0,0 +1,10 @@
+package com.baizhi.domain;
+
+/**
+ * Created by HGM on 2018/6/12.
+ */
+public class Test1 {
+    public static void  main(String []args){
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World!My Name is HGM .....</h2>
+</body>
+</html>
